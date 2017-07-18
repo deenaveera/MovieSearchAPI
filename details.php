82c40d9e1b0e3c194d3d985d08640e996b87c5db
m2vd <dthayalan333@gmail.com>
2017-07-18 16:53:04 +0530

    code beautify completed
    code beautify completed

--- details.php
+++ details.php
@@ -6,115 +6,159 @@ $jsonresponse = $obj->CURL($term);
 //$json = json_encode($jsonresponse);
 ?>
 
-<div align="center" style="margin-top:100px;">
-<h1>Movie Details</h1>
-<?php
+    <div align="center" style="margin-top:100px;">
+        <h1>Movie Details</h1>
+        <?php
 if(!empty($jsonresponse)){ ?>
-<table style="width:40%">
-  <tr>
-    <th>Title:</th>
-    <td><?php echo $jsonresponse['Title']; ?></td>
-  </tr>
-  <tr>
-    <th>Year:</th>
-    <td><?php echo $jsonresponse['Year']; ?></td>
-  </tr>
-  <tr>
-    <th>Rated:</th>
-    <td><?php echo $jsonresponse['Rated']; ?></td>
-  </tr>
-  <tr>
-    <th>Runtime:</th>
-    <td><?php echo $jsonresponse['Runtime']; ?></td>
-  </tr>
-  <tr>
-    <th>Genre:</th>
-    <td><?php echo $jsonresponse['Genre']; ?></td>
-  </tr>
-  <tr>
-    <th>Director:</th>
-    <td><?php echo $jsonresponse['Director']; ?></td>
-  </tr>
-  <tr>
-    <th>Writer:</th>
-    <td><?php echo $jsonresponse['Writer']; ?></td>
-  </tr>
-  <tr>
-    <th>Actors:</th>
-    <td><?php echo $jsonresponse['Actors']; ?></td>
-  </tr>
-  <tr>
-    <th>Plot:</th>
-    <td><?php echo $jsonresponse['Plot']; ?></td>
-  </tr>
-  <tr>
-    <th>Language:</th>
-    <td><?php echo $jsonresponse['Language']; ?></td>
-  </tr>
-  <tr>
-    <th>Country:</th>
-    <td><?php echo $jsonresponse['Country']; ?></td>
-  </tr>
-   <tr>
-    <th>Poster:</th>
-    <td>
-	<img src="<?php echo $jsonresponse['Poster']; ?>" alt="<?php echo $jsonresponse['Title']; ?>" height="60" width="60">
-	</td>
-  </tr>
-  <tr>
-    <th>Metascore:</th>
-    <td><?php echo $jsonresponse['Metascore']; ?></td>
-  </tr>
-  <tr>
-    <th>imdbRating:</th>
-    <td><?php echo $jsonresponse['imdbRating']; ?></td>
-  </tr>
-  <tr>
-    <th>imdbVotes:</th>
-    <td><?php echo $jsonresponse['imdbVotes']; ?></td>
-  </tr>
-  <tr>
-    <th>Type:</th>
-    <td><?php echo $jsonresponse['Type']; ?></td>
-  </tr>
-  <tr>
-    <th>DVD:</th>
-    <td><?php echo $jsonresponse['DVD']; ?></td>
-  </tr>
-  <tr>
-    <th>BoxOffice:</th>
-    <td><?php echo $jsonresponse['BoxOffice']; ?></td>
-  </tr>
-  <tr>
-    <th>Production:</th>
-    <td><?php echo $jsonresponse['Production']; ?></td>
-  </tr>
-  <tr>
-    <th>Website:</th>
-    <td><?php echo $jsonresponse['Website']; ?></td>
-  </tr>
-  <tr>
-    <th>Response:</th>
-    <td><?php echo $jsonresponse['Response']; ?></td>
-  </tr>
-</table>
-<?php 
+            <table style="width:40%">
+                <tr>
+                    <th>Title:</th>
+                    <td>
+                        <?php echo $jsonresponse['Title']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Year:</th>
+                    <td>
+                        <?php echo $jsonresponse['Year']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Rated:</th>
+                    <td>
+                        <?php echo $jsonresponse['Rated']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Runtime:</th>
+                    <td>
+                        <?php echo $jsonresponse['Runtime']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Genre:</th>
+                    <td>
+                        <?php echo $jsonresponse['Genre']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Director:</th>
+                    <td>
+                        <?php echo $jsonresponse['Director']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Writer:</th>
+                    <td>
+                        <?php echo $jsonresponse['Writer']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Actors:</th>
+                    <td>
+                        <?php echo $jsonresponse['Actors']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Plot:</th>
+                    <td>
+                        <?php echo $jsonresponse['Plot']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Language:</th>
+                    <td>
+                        <?php echo $jsonresponse['Language']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Country:</th>
+                    <td>
+                        <?php echo $jsonresponse['Country']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Poster:</th>
+                    <td>
+                        <img src="<?php echo $jsonresponse['Poster']; ?>" alt="<?php echo $jsonresponse['Title']; ?>" height="60" width="60">
+                    </td>
+                </tr>
+                <tr>
+                    <th>Metascore:</th>
+                    <td>
+                        <?php echo $jsonresponse['Metascore']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>imdbRating:</th>
+                    <td>
+                        <?php echo $jsonresponse['imdbRating']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>imdbVotes:</th>
+                    <td>
+                        <?php echo $jsonresponse['imdbVotes']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Type:</th>
+                    <td>
+                        <?php echo $jsonresponse['Type']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>DVD:</th>
+                    <td>
+                        <?php echo $jsonresponse['DVD']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>BoxOffice:</th>
+                    <td>
+                        <?php echo $jsonresponse['BoxOffice']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Production:</th>
+                    <td>
+                        <?php echo $jsonresponse['Production']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Website:</th>
+                    <td>
+                        <?php echo $jsonresponse['Website']; ?>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Response:</th>
+                    <td>
+                        <?php echo $jsonresponse['Response']; ?>
+                    </td>
+                </tr>
+            </table>
+            <?php 
 } 
 else{
 	echo 'Movie Details Not Available';
 }
 ?>
 
-</div>
+    </div>
 
-<style>
-table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-	background-color: #EBECEB;
-}
-th, td {
-    padding: 5px;
-    text-align: left;
-}
-</style>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            background-color: #EBECEB;
+        }
+
+        th,
+        td {
+            padding: 5px;
+            text-align: left;
+        }
+    </style>
--- index.php
+++ index.php
@@ -1,52 +1,59 @@
 <?php
 require 'config.php';
 ?>
-<html>
-<head>
-<title>Search Movies</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-<style>
-.glyphicon {
-    left: 380px !important;
-}
-.list_item_container {
-    width:300px;
-    height: 60px;
-    padding: 5px 0;
-}
-.image {
-    width: 60px;
-    height: 60px;
-    margin-right: 10px;
-    float: left;
-}
-.description {
-    font-style: italic;
-    font-size: 0.8em;
-    color: gray;
-}
-.label,.description{
-	color: #000;
-}
-</style>
-</head>
-<body>
-<form method="GET" action="index.php" > 
-<div class="ui-widget" align="center" style="margin-top:120px;">
-    <label for="term">Movies: </label>
-    <span class="glyphicon glyphicon-search"></span>
-	<input name="term" id="term" placeholder="Search" size="40" style="border: 2px solid #a1a1a1;">
-</div>
-</form>
+    <html>
+
+    <head>
+        <title>Search Movies</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <style>
+            .glyphicon {
+                left: 380px !important;
+            }
+
+            .list_item_container {
+                width: 300px;
+                height: 60px;
+                padding: 5px 0;
+            }
+
+            .image {
+                width: 60px;
+                height: 60px;
+                margin-right: 10px;
+                float: left;
+            }
+
+            .description {
+                font-style: italic;
+                font-size: 0.8em;
+                color: gray;
+            }
+
+            .label,
+            .description {
+                color: #000;
+            }
+        </style>
+    </head>
+
+    <body>
+        <form method="GET" action="index.php">
+            <div class="ui-widget" align="center" style="margin-top:120px;">
+                <label for="term">Movies: </label>
+                <span class="glyphicon glyphicon-search"></span>
+                <input name="term" id="term" placeholder="Search" size="40" style="border: 2px solid #a1a1a1;">
+            </div>
+        </form>
 
 
 
-<?php
+        <?php
 
 if(!empty($_GET)){
 $term = $_GET['term'];
@@ -54,40 +61,45 @@ $obj = new Config();
 $jsonresponse = $obj->CURL($term);
 ?>
 
-<div>
-<table align="center">
-<tr>
-<td><a href="<?php echo $base_url.'details.php?term='.$jsonresponse['Title']; ?>"><img src="<?php echo $jsonresponse['Poster']; ?>" alt="<?php echo $jsonresponse['Title']; ?>" height="100" width="100"></a></td>
-</tr>
-<tr>
-<td><a href="<?php echo $base_url.'details.php?term='.$jsonresponse['Title']; ?>"><?php echo $jsonresponse['Title']; ?></a></td>
-</tr>
-</table>
+            <div>
+                <table align="center">
+                    <tr>
+                        <td><a href="<?php echo $base_url.'details.php?term='.$jsonresponse['Title']; ?>"><img src="<?php echo $jsonresponse['Poster']; ?>" alt="<?php echo $jsonresponse['Title']; ?>" height="100" width="100"></a></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <a href="<?php echo $base_url.'details.php?term='.$jsonresponse['Title']; ?>">
+                                <?php echo $jsonresponse['Title']; ?>
+                            </a>
+                        </td>
+                    </tr>
+                </table>
 
-</div>
-	
-<?php } ?>
+            </div>
+
+            <?php } ?>
 
 
 
 
 
-</body>
-</html>
+    </body>
 
-<script>
-$(function() {
-	var baseurl = '<?php echo $base_url; ?>';
-	$("#term").autocomplete({
-		source: "search.php",
-		minLength: 3,
-	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-			var inner_html = '<a href="'+item.detailurl+'"><div class="list_item_container"><div class="image"><img width="60" height="60" src="' + item.thumbnail + '"></div><div class="label">' + item.label + '</div><div class="description"></div></div></a>';
-			return $( "<li></li>" )
-				.data( "item.autocomplete", item )
-				.append(inner_html)
-				.appendTo( ul );
-		};
-   
-});
-</script>
+    </html>
+
+    <script>
+        $(function() {
+            var baseurl = '<?php echo $base_url; ?>';
+            $("#term").autocomplete({
+                source: "search.php",
+                minLength: 3,
+            }).data("ui-autocomplete")._renderItem = function(ul, item) {
+                var inner_html = '<a href="' + item.detailurl + '"><div class="list_item_container"><div class="image"><img width="60" height="60" src="' + item.thumbnail + '"></div><div class="label">' + item.label + '</div><div class="description"></div></div></a>';
+                return $("<li></li>")
+                    .data("item.autocomplete", item)
+                    .append(inner_html)
+                    .appendTo(ul);
+            };
+
+        });
+    </script>
