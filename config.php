b90cc810e8acd305265fabab14c2877ca1532be4
m2vd <dthayalan333@gmail.com>
2017-07-18 16:17:24 +0530

    final commited
    final commited

---
+++ config.php
@@ -0,0 +1,20 @@
+<?php
+$base_url = "http://" . $_SERVER['SERVER_NAME'] . '/search-api/';
+
+class Config {
+	
+	public function CURL($term)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_URL,'http://www.omdbapi.com/?t='.$term.'&apikey=ec6483bd');
+		$result=curl_exec($ch);
+		curl_close($ch);
+		$jsonresponse = json_decode($result, true);
+		return $jsonresponse;
+	}
+}
+
+?>
+
---
+++ search.php
@@ -0,0 +1,17 @@
+<?php
+require 'config.php';
+$term = $_GET['term'];
+$obj = new Config();
+$jsonresponse = $obj->CURL($term);
+if($jsonresponse['Response'] == True)
+{
+	$title = $jsonresponse['Title'];
+	$thumbnail = $jsonresponse['Poster'];
+	$result = array(
+				array('label' => $title, 'value' => $title, 'thumbnail' => $thumbnail, 'detailurl' => $base_url.'details.php?term='.$title)
+			  );
+	echo json_encode($result);
+}
+
+?>
+
